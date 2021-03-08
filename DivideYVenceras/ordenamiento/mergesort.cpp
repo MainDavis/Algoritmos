fdc21a1c9f799ced2fbe211f25b8769d0b335cec
MainDavis <dayamon12500@gmail.com>
2021-03-08 23:15:40 +0100

    Terminado

--- DivideYVenceras/ordenamiento/mergesort.cpp
+++ DivideYVenceras/ordenamiento/mergesort.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 void  combinar (int a[ ] , int len_a , int b[ ] , int len_b , int c[ ])
 {
-	cout << "["<< a[0] << ", " << a[1] << "]  ";
-	cout << "["<< b[0] << ", " << b[1] << "]";
 	int posA=0,posB=0;
   	for(int i=0; i<(len_a+len_b); i++){
   		
@@ -21,22 +19,24 @@ void  combinar (int a[ ] , int len_a , int b[ ] , int len_b , int c[ ])
 			c[i]=b[posB];
 			posB++;	
 		}
-		cout << "(" << c[i] << ")";
+
 	}
-	cout << "\n";
 }
 
 
 void ordenarMergeSort (int x[], int len)
 {
 	int temp[len];
-	if(len == 1) return;
-	else{
-    	/*Ordenar dos mitades */
-    	ordenarMergeSort(x, len/2);
-		ordenarMergeSort(x+(len/2), len/2);      	
+	int mitad = len/2;
+	if(len == 1) {
+		return;
+	}else{
+    	/*Ordenar dos mitades */    	
+		ordenarMergeSort(x, mitad);
+		ordenarMergeSort(x+mitad, len-mitad);
+    			    	
       	/*Combinar ambas mitades y guar;darlo en temp */
-		combinar(x, len, x+(len/2)+1, len, temp);
+		combinar(x, mitad, x+(mitad), (len-mitad), temp);
       	/*Copiar del array temporal al array resultado */
       	for (int k = 0; k < len; k++){
 	  		x[k] = temp[k];
@@ -48,13 +48,12 @@ void ordenarMergeSort (int x[], int len)
 
 
 
-int main()
-{
- int n;
- cin>>n;
- int *x=new int[n];
- for(int i=0;i<n;i++)
-   cin>>x[i];
- ordenarMergeSort(x,n);
+int main(){
+	int n;
+	cin>>n;
+	int *x=new int[n];
+	for(int i=0;i<n;i++)
+		cin>>x[i];
+	if(n>0) ordenarMergeSort(x,n);
 }
 
