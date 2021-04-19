4fece8b6cc32d5b561e63edb2627f020c1c4e3e2
MainDavis <dayamon12500@gmail.com>
2021-04-19 21:47:49 +0200

    HasMaps inicial

--- pruebas/pruebas.cpp
+++ pruebas/pruebas.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 using namespace std;
 
-void imprimir(int arr[]);
-
 int main(){
-    int n[10] = {1,2,3,4,5,6,7,8,9,0};
-    imprimir(n+1);
-return 0;
+    
+    int a;
+
+    while(true){
+        a++;
+        cout << "[" << a << "]  -> ";
+        system("pause");
+    }
+
+    return 0;
 }
 
-void imprimir(int arr[]){
-	cout << arr[0];
-}
