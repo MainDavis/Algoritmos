ca948e17f280ac281e018edb4e867748310b80f5
MainDavis <dayamon12500@gmail.com>
2021-04-27 01:50:48 +0200

    Supermercado terminado

--- pruebas/pruebas.cpp
+++ pruebas/pruebas.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
     
-    long n;
-    cin >> n;
-
-    cout << n;
+    int a = 0, b = 1;
+    
+    if(a&1) cout << "A impar\n";
+    else cout << "A par\n";
+    
+    if(b&1) cout << "B impar\n";
+    else cout << "B par\n";
 
 }
 
